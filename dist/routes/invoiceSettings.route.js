d845bd188e5afa904430d79298dee31836473df6
shawneha992-byte <shawneha992@gmail.com>
2026-03-31 16:17:25 +0530

    cors error

--- dist/routes/invoiceSettings.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const invoiceSettings_controller_1 = require("../controllers/invoiceSettings.controller");
-const router = (0, express_1.Router)();
-router.get("/", invoiceSettings_controller_1.getInvoiceSettings); // GET  /api/invoice-settings
-router.post("/", invoiceSettings_controller_1.saveInvoiceSettings); // POST /api/invoice-settings
-exports.default = router;
--- dist/routes/proforma.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const proforma_controller_1 = require("../controllers/proforma.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const router = (0, express_1.Router)();
-router.post("/", auth_middleware_1.authMiddleware, proforma_controller_1.createProformaInvoice);
-router.get("/", auth_middleware_1.authMiddleware, proforma_controller_1.getProformaInvoices);
-exports.default = router;
